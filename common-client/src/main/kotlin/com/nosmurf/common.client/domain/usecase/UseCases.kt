d8757a7e62c7fd21b63285d501a8ac966a776311
Daniel Llanos Muñoz <daniel.llanosmunoz@gmail.com>
2019-05-13 17:05:14 +0200

    Added client usecases

--- common-client/src/main/kotlin/com/nosmurf/common.client/domain/usecase/UseCases.kt
+++ common-client/src/main/kotlin/com/nosmurf/common.client/domain/usecase/UseCases.kt
@@ -1,2 +1,17 @@
 package com.nosmurf.common.client.domain.usecase
 
+import com.nosmurf.common.client.domain.repository.Repository
+import com.nosmurf.model.Comment
+import com.nosmurf.model.Movie
+import com.nosmurf.model.MovieDetail
+import com.nosmurf.model.NewComment
+
+suspend fun getMovies(repository: Repository): List<Movie> = repository.getMovies()
+
+suspend fun getMovie(id: Long, repository: Repository): MovieDetail = repository.getMovie(id)
+
+suspend fun getComments(movieId: Long, repository: Repository): List<Comment> =
+        repository.getComments(movieId)
+
+suspend fun addNewComment(newComment: NewComment, movieId: Long, repository: Repository): List<Comment> =
+        repository.addNewComment(newComment, movieId)
