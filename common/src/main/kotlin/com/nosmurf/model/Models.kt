2003407502f78b07aeb758da8bd6df67d166a1e9
Daniel Llanos Muñoz <daniel.llanosmunoz@gmail.com>
2019-05-13 13:30:05 +0200

    Added common models

--- common/src/main/kotlin/com/nosmurf/model/Models.kt
+++ common/src/main/kotlin/com/nosmurf/model/Models.kt
@@ -1,2 +1,49 @@
 package com.nosmurf.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Movie(
+        val id: String,
+        val title: String,
+        val img: String,
+        val classification: String,
+        val schedule: List<Schedule>
+)
+
+@Serializable
+data class MovieDetail(
+        val title: String,
+        val image: String,
+        val country: String,
+        val year: String,
+        val duration: String,
+        val director: String,
+        val releaseDate: String,
+        val classification: String,
+        val cast: List<Actor>,
+        val sinopsis: String,
+        val trailer: String,
+        val tickets: List<Schedule>
+)
+
+@Serializable
+data class Actor(
+        val name: String,
+        val image: String
+)
+
+@Serializable
+data class Schedule(
+        val time: String,
+        val paymentUrl: String,
+        val price: String
+)
+
+
+@Serializable
+data class Comment(
+        val id: Long,
+        val createdDate: Long,
+        val value: String
+)
